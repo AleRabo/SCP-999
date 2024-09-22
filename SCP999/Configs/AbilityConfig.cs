7a36e7b6691d8997ee39ac43ec1a77d5055efe34
AleRabo <83182793+AleRabo@users.noreply.github.com>
2024-09-22 16:36:34 +0200

    Update AbilityConfig.cs

--- SCP999/Configs/AbilityConfig.cs
+++ SCP999/Configs/AbilityConfig.cs
@@ -1,17 +1,19 @@
-﻿namespace SCP999;
+namespace SCP999;
 public class AbilityConfig
 {
     // Properties for Range and Effect Duration
-    public float Range { get; set; }
+    public byte Intensity { get; set; }
     public float EffectDuration { get; set; }
+    public bool ScpFriendly { get; set; }
 
     // Default constructor (required for YAML deserialization)
     public AbilityConfig() { }
 
     // Constructor with parameters (for manual creation)
-    public AbilityConfig(float range, float effectDuration)
+    public AbilityConfig(byte intensity, float effectDuration, bool scpFriendly)
     {
-        Range = range;
+        Intensity = intensity;
         EffectDuration = effectDuration;
+        ScpFriendly = scpFriendly;
     }
 }
